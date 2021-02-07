c70a6ceffc578d6e74761db14927696e5dfc2728
chubi-x <michaeladejoh01@gmail.com>
2021-02-07 17:00:53 +0400

    Added middleware to update items in collection

--- server.js
+++ server.js
@@ -39,13 +39,35 @@ app.get("/:collectionName",(req,res,next)=>{
 
 //middleware to post items to collection
 app.post("/:collectionName", (req,res,next)=>{
-    req.collection.insert(req.body,(err,result)=>{
+    req.collection.insertOne(req.body,(err,result)=>{
         if(err) return next(err);
         res.send(result.ops);
     }
     );
 })
+//middleware to retrieve items by object ID
+const ObjectID = require('mongodb').ObjectID;
+app.get("/:collectionName/:id",(req,res,next)=>{
+    req.collection.findOne(
+        { _id:new ObjectID(req.params.id)}, (e,result)=>{
+                if(e) return next(e);
+                res.send(result);
+            }
+    )
+});
 
+//middleware to update items in collection
+app.put("/:collectionName/:id",(req,res,next)=>{
+    req.collection.update(
+        {_id: new ObjectID(req.params.id)},
+        {$set:req.body},
+        {safe:true,multi:false},
+        (e,result)=>{
+            if(e) return next(e);
+            res.send((result.result.n===1? {msg:"success"} : {msg:"error"}))
+        }
+    )
+})
 //define a port
 const port = process.env.PORT || 3000;
 //run the server
